d486a495d175ea559c46eca722dfc3d8da37ad50
Steven Fusco <slf@stevenfusco.com>
2025-07-12 13:07:15 +0000

    testing motor

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
-from flask import Flask, send_from_directory, jsonify
-from gpiozero import LED
-import os
-import signal
-import atexit
-from time import sleep
+from gpiozero import DigitalOutputDevice
+from gpiozero.pins.lgpio import LGPIOFactory
+import time
 
-led = LED(18)
-led.on()
-sleep(3)
-led.off()
-sleep(3)
-led.close()
+factory = LGPIOFactory()
+
+dir_pin = DigitalOutputDevice(17, pin_factory=factory)  # DIR−
+pul_pin = DigitalOutputDevice(27, pin_factory=factory)  # PUL−
+
+dir_pin.on()
+
+for _ in range(20000):
+    pul_pin.off()
+    time.sleep(0.001)
+    pul_pin.on()
+    time.sleep(0.001)
 
